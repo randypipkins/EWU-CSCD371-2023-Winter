995e1fb7c42b6397af8e2c0e2554d090a19decc7
Randy Pipkins <rpipkins@ewu.edu>
2023-02-12 12:52:42 -0800

    Assignment 5 completed

---
+++ GenericsHomework/GenericsHomework/Node.cs
@@ -0,0 +1,76 @@
+﻿namespace GenericsHomework;
+
+public class Node<TVal> where TVal : notnull
+{
+    public Node(TVal val)
+    {
+        Val = val ?? throw new ArgumentNullException($"Node<{typeof(TVal)}> value cannot be null");
+        Next = this;
+    }
+
+    public TVal Val
+    {
+        get;
+        set;
+    }
+
+    public Node<TVal> Next
+    {
+        get;
+        private set;
+    }
+
+    public void Append(TVal node)
+    {
+        if(Exists(node))
+        {
+            throw new ArgumentException("This value already exists. Value must be unique.");
+        }
+
+        Node<TVal> node1 = new(node);
+
+        if(Next == this)
+        {
+            Next = node1;
+            node1.Next = this;
+        }
+        else
+        {
+            node1.Next = Next;
+            Next = node1;
+        }
+    }
+
+    /*
+     * Need only to get rid of the link from the first
+     * node because only the first node can access the subsequent nodes,
+     * thus leaving no more references to the objects created.
+     * This is proven in GenericsHomeworkTests.Node_GarbageCollection()
+     */
+
+    public void Clear()
+    {
+        Next = this;
+    }
+
+    public Boolean Exists(TVal val)
+    {
+        Node<TVal> temp = this;
+        while (!temp.Val.Equals(val))
+        {
+            if(temp.Next == this)
+            {
+                return false;
+            }
+
+            temp = temp.Next;
+        }
+
+        return true;
+    }
+
+    public override string ToString()
+    {
+        return $"{Val}";
+    }
+}
---
+++ GenericsHomework/GenericsHomeworkTests/NodeTests.cs
@@ -0,0 +1,96 @@
+using GenericsHomework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace GenericsHomeworkTests
+{
+    [TestClass]
+    public class NodeTests
+    {
+        [TestMethod]
+        public void Node_ReturnsVal()
+        {
+            Node<int> node = new(42);
+            Assert.AreEqual(42, node.Val);
+        }
+
+        [TestMethod]
+        public void Node_OverridesToString_Pass()
+        {
+            Node<int> node = new(42);
+            Assert.AreEqual("42", node.Val.ToString());
+        }
+
+        [TestMethod]
+        public void Node_AppendThrowsExceptionIfValExists()
+        {
+            Node<int> node = testHelper();
+            Assert.ThrowsException<ArgumentException>(() => node.Append(44));
+        }
+
+        [TestMethod]
+        public void Node_NextPropertyReferenceNextNode()
+        {
+            Node<int> node = new(42);
+            node.Append(43);
+
+            Assert.AreEqual<int>(43, node.Next.Val);
+        }
+
+        [TestMethod]
+        public void Node_AppendInsertsAfterOriginalNode()
+        {
+            Node<int> node = testHelper();
+            Assert.AreEqual<int>(45, node.Next.Val);
+        }
+
+        [TestMethod]
+        public void Node_MaintainsCircularLinkWithFourNodes()
+        {
+            Node<int> node = testHelper();
+            Assert.AreEqual<int>(42, node.Next.Next.Next.Next.Val);
+        }
+
+        [TestMethod]
+        public void Node_NextPropertyReferencesSelf()
+        {
+            Node<int> node = new(42);
+            Assert.AreEqual<int>(42, node.Next.Val);
+        }
+
+        [TestMethod]
+        public void Node_GarbageCollection()
+        {
+            Node<int> node = testHelper();
+            WeakReference nodeRef = GarbageCollectionTestHelper(node);
+            node.Clear();
+
+            GC.Collect();
+            bool isAlive = nodeRef.IsAlive;
+
+            Assert.IsFalse(isAlive);
+        }
+
+        [TestMethod]
+        public void Node_GenericsConstructorThrowsErrorIfPassedNull()
+        {
+            Action action = () => new Node<string>(null!);
+            Assert.ThrowsException<ArgumentNullException>(() => action.Invoke());
+        }
+
+        public Node<int> testHelper()
+        {
+            Node<int> node = new(42);
+            node.Append(43);
+            node.Append(44);
+            node.Append(45);
+            return node;
+        }
+
+        public WeakReference GarbageCollectionTestHelper(Node<int> node)
+        {
+            WeakReference nodeRef = new(node.Next);
+            return nodeRef;
+        }
+    }
+}
